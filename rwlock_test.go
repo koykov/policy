e6c35c589acd05146122626211468389cd3aa44c
Konstantin Voykov <koykov0@gmail.com>
2020-11-08 00:18:33 +0200

    Cover rwlock policy with bench to measure difference between Locked and LockFree policies.

---
+++ rwlock_test.go
@@ -0,0 +1,68 @@
+package policy
+
+import (
+	"math/rand"
+	"testing"
+)
+
+const (
+	testStageCapRW = 1000
+)
+
+type testStageRW struct {
+	lock RWLock
+	data map[int32]int32
+}
+
+func (s *testStageRW) Fill(step int) {
+	if step < 0 || step >= testStageCap {
+		step = 1
+	}
+	s.lock.Lock()
+	for i := 0; i < testStageCap; i += step {
+		s.data[int32(i)] = rand.Int31()
+	}
+	s.lock.Unlock()
+}
+
+func (s *testStageRW) Write() {
+	s.lock.Lock()
+	s.data[rand.Int31n(testStageCap)] = rand.Int31()
+	s.lock.Unlock()
+}
+
+func (s *testStageRW) Read() {
+	s.lock.RLock()
+	v, ok := s.data[rand.Int31n(testStageCap)]
+	s.lock.RUnlock()
+	_, _ = v, ok
+}
+
+func BenchmarkRWLockPolicyLocked(b *testing.B) {
+	stage := testStageRW{data: make(map[int32]int32, testStageCapRW)}
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if rand.Float64() < 0.5 {
+				stage.Read()
+			} else {
+				stage.Write()
+			}
+		}
+	})
+}
+
+func BenchmarkRWLockPolicyLockFree(b *testing.B) {
+	stage := testStageRW{data: make(map[int32]int32, testStageCapRW)}
+	stage.Fill(2)
+	stage.lock.SetPolicy(LockFree)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			stage.Read()
+		}
+	})
+}
