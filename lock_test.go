e11207f0548d7c8c215c99a32d1a397da5a195f1
Konstantin Voykov <koykov0@gmail.com>
2020-11-08 00:16:01 +0200

    Cover lock policy with bench to measure difference between Locked and LockFree policies.

---
+++ lock_test.go
@@ -0,0 +1,68 @@
+package policy
+
+import (
+	"math/rand"
+	"testing"
+)
+
+const (
+	testStageCap = 1000
+)
+
+type testStage struct {
+	lock Lock
+	data map[int32]int32
+}
+
+func (s *testStage) Fill(step int) {
+	if step < 0 || step >= testStageCap {
+		step = 1
+	}
+	s.lock.Lock()
+	for i := 0; i < testStageCap; i += step {
+		s.data[int32(i)] = rand.Int31()
+	}
+	s.lock.Unlock()
+}
+
+func (s *testStage) Write() {
+	s.lock.Lock()
+	s.data[rand.Int31n(testStageCap)] = rand.Int31()
+	s.lock.Unlock()
+}
+
+func (s *testStage) Read() {
+	s.lock.Lock()
+	v, ok := s.data[rand.Int31n(testStageCap)]
+	s.lock.Unlock()
+	_, _ = v, ok
+}
+
+func BenchmarkLockPolicyLocked(b *testing.B) {
+	stage := testStage{data: make(map[int32]int32, testStageCap)}
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if rand.Float64() < 0.5 {
+				stage.Read()
+			} else {
+				stage.Write()
+			}
+		}
+	})
+}
+
+func BenchmarkLockPolicyLockFree(b *testing.B) {
+	stage := testStage{data: make(map[int32]int32, testStageCap)}
+	stage.Fill(2)
+	stage.lock.SetPolicy(LockFree)
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			stage.Read()
+		}
+	})
+}
